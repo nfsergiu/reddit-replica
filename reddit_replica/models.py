d5108df76a9030bc5c690e56dd4a864df92f3268
nfsergiu <sern@bendingspoons.com>
2021-10-05 18:02:46 +0300

    feat: add database connection (mysql)

--- reddit_replica/models.py
+++ reddit_replica/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    id_user = models.IntegerField(primary_key=True, unique=True, db_index=True)
+    username = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
